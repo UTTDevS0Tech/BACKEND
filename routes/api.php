e926fd2fd322fe5586c3fb0a39e644964fee8ae9
Meredith Judith Valdivia Hernandez <24170085@uttcampus.edu.mx>
2026-03-06 09:07:20 -0600

    repo correcto lol

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TipoServicioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
+
+
+Route::apiResource('servicios', ServicioController::class);
+//GET /api/servicios
+//GET /api/servicios/{id}
+//POST /api/servicios
+//PUT /api/servicios/{id}
+//DELETE /api/servicios/{id}
