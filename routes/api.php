7c925356d23e5a6af61b336f887b4fc92e60e410
Meredith Judith Valdivia Hernandez <24170085@uttcampus.edu.mx>
2026-03-12 15:26:03 -0600

    UserController PT1

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TipoServicioController;
+use App\Http\Controllers\UserController;
 
+//USER
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('users', [UserController::class, 'index']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::post('users', [UserController::class, 'store']);
 
+//AUTH
 Route::post('/register', [AuthController::class, 'register']);
 
-
+//SERVICIOS
 Route::apiResource('servicios', ServicioController::class);
 //GET /api/servicios
 //GET /api/servicios/{id}
@@ -19,5 +25,12 @@ Route::apiResource('servicios', ServicioController::class);
 //PUT /api/servicios/{id}
 //DELETE /api/servicios/{id}
 Route::patch('servicio/{id}/toggle', [ServicioController::class, 'toggle']);
+
+//TIPO SERVICIOS
 Route::apiResource('tipo-servicios', TipoServicioController::class);
+//GET /api/tipo-servicios
+//GET /api/tipo-servicios/{id}
+//POST /api/tipo-servicios
+//PUT /api/tipo-servicios/{id}
+//DELETE /api/tipo-servicios/{id}
 Route::patch('tipo-servicios/{id}/toggle-status', [TipoServicioController::class, 'toggleStatus']);
