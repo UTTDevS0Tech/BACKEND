c21e697b08abb485fa52b5ecc2fc49ee68d75b63
diego <dieguito.reyes.14xd@gmail.com>
2026-03-30 23:41:02 -0600

    ya deberia hacerlo los calculos para los horarios el backend;

--- app/Http/Controllers/CitaController.php
+++ app/Http/Controllers/CitaController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Cita;
 use App\Models\Cliente;
 use App\Http\Resources\CitaResource;
+use Carbon\Carbon;
 use App\Traits\ApiResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\CitaRequest;
@@ -13,6 +14,7 @@ use App\Http\Requests\CitaUpdateRequest;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Models\TipoServicio;
 
 
 class CitaController extends Controller
@@ -78,12 +80,40 @@ class CitaController extends Controller
 
         }
 
+        $horaInicio = Carbon::parse($request->hora_c);
+
+        $minutosTotales = collect($request->detalle_cita)->reduce(function ($api, $item){
+            $servicio = TipoServicio::find($item['tipo_servicio_id']);
+            return $api + $servicio->tiempo_duracion;
+        }, 0);
+
+        $horaFin = $horaInicio->copy()->addMinutes($minutosTotales);
+
+
+
+
+
+
+        $chocaonochocaconotrohorario = Cita::where('personal_id', $request->persona_id)
+        ->where('fecha_c', $request->fecha_c)
+        ->where('estado', '!=', 'cancelada')
+        ->where(function($query) use ($horaInicio, $horaFin){
+            $query->whereBetween('hora_c', [$horaInicio->format('H:i'), $horaFin->format('H:i')])
+            ->orWhereBetween('hora_fin', [$horaInicio->format('H:i'), $horaFin->format('H:i')]);
+        })
+        
+        ->exists();
+
+        if($chocaonochocaconotrohorario) {
+            return $this->apiResponse(null, 'ese horario ya ta busy compare');
+        }
         $data = $request->validated();
         $servicios = $data['detalle_cita'];
 
         unset($data['detalle_cita']); //no se la compliquen es basicemten por q no hay campo detalle_cita pero ocupas guardar los servicios entonces es como "ey guardamos los servicios pero al llegar a la cita es como "compare tu no tienes el campo" okay entonces bay bay y ya se los metemos a la detalle_citas
         $data['cliente_id'] = $clienteabuscar->id;
         $data['estado'] = 'pendiente';
+        $data['hora_fin'] = $horaFin->format('H:i');
 
 
     $citaweb = Cita::create($data);
