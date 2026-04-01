8eaea9f47139512eca940635ed9ab5b7e47b6a3e
diego <dieguito.reyes.14xd@gmail.com>
2026-04-01 14:01:17 -0600

    probar

--- app/Http/Controllers/PersonalController.php
+++ app/Http/Controllers/PersonalController.php
@@ -16,7 +16,7 @@ class PersonalController extends Controller
 
     public function index() {
         $estilista = Personal::with('horarios')->whereHas('user.rol', function($query) {
-            $query -> where('nombre', 1); 
+            $query -> where('nombre', 'Estilista'); 
         })->get();
             return $this->apiResponse(PersonalResource::collection($estilista),'estilistas' ,200);
         }
