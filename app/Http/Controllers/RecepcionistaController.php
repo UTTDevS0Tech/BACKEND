dd0bc81b9b432c2af45079bcdc8204d7b4564f34
SaraSaldivar <sarasaldivar472@gmail.com>
2026-04-06 13:40:37 -0600

    ojala jale

--- app/Http/Controllers/RecepcionistaController.php
+++ app/Http/Controllers/RecepcionistaController.php
@@ -38,6 +38,8 @@ class RecepcionistaController extends Controller
                 $q->where('nom', 'like', "%{$search}%")
                 ->orWhere('apellido_p', 'like', "%{$search}%")
                 ->orWhere('apellido_m', 'like', "%{$search}%");
+                $q->orWhereRaw("CONCAT(nom, ' ', apellido_p, ' ', apellido_m) LIKE ?",["%{$search}%"]);
+
             });
 
         }
