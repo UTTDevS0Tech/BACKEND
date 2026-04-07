002562456864f6b8a3027a3ecc26f5dd71a77470
Carlos D <carlosdanielgrdz@gmail.com>
2026-04-07 03:57:03 -0600

    cambioss

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -9,19 +9,12 @@
 {{-- Body --}}
 {!! $slot !!}
 
-{{-- Subcopy --}}
-@isset($subcopy)
-<x-slot:subcopy>
-<x-mail::subcopy>
-{!! $subcopy !!}
-</x-mail::subcopy>
-</x-slot:subcopy>
-@endisset
+
 
 {{-- Footer --}}
 <x-slot:footer>
 <x-mail::footer>
-© {{ date('Y') }} {{ config('app.name') }}. {{ __('All rights reserved.') }}
+© 2026 Estetica Nova. Todos los derechos reservados.
 </x-mail::footer>
 </x-slot:footer>
 </x-mail::layout>
