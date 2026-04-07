d05483446a86db0d4894620916cd21f4af3b26be
Carlos D <carlosdanielgrdz@gmail.com>
2026-04-07 03:32:20 -0600

    aver si se ve el logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo-v2.1.png" class="logo" alt="Laravel Logo">
+<img src="https://api.carlosd-dev.me/images/logo.png" class="logo" alt="Nova Logo">
 @else
 {!! $slot !!}
 @endif
