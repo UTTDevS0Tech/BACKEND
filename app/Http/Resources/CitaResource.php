6b0eeda8443b55db485be3d3614605bf64c193bf
SaraSaldivar <sarasaldivar472@gmail.com>
2026-04-09 19:23:02 -0600

    i like to move it move it

--- app/Http/Resources/CitaResource.php
+++ app/Http/Resources/CitaResource.php
@@ -14,13 +14,22 @@ class CitaResource extends JsonResource
             'id' => $this->id,
             'apartado' => $this->apartado,
             'total' => $this->total,
+            'personal_id' => $this->personal_id,
             'personal' => $this->personal?->nombre ?? 'sin asignar',
             'hora_c' => $this->hora_c,
             'hora_fin' => $this->hora_fin,
             'fecha_c' => $this->fecha_c,
             'estado' => $this->estado,
+            'cliente_id' => $this->cliente_id,
             'cliente' => trim(($this->cliente?->nom ?? '') . ' ' .($this->cliente?->apellido_p ?? '') . ' ' .($this->cliente?->apellido_m ?? '')) ?: 'desconocido',
-            'detalle_cita' => DetalleCitaResource::collection($this->whenLoaded('detalles')),
+            'detalles' => $this->detalles->map(function ($detalle) {
+            return [
+                'servicio_id' => $detalle->tipo_servicio_id,
+                'subtotal' => $detalle->precio_capturado ?? $detalle->subtotal,
+                'servicio' => $detalle->tipoServicio?->nombre,
+            ];
+        })->values(),        
+        
         ];
     }
 }
