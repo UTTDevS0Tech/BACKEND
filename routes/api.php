40a1a688028f80ee75519319308e1d2c56252bbc
diego <dieguito.reyes.14xd@gmail.com>
2026-04-10 22:35:50 -0600

    cambie la ruta a publica la de servicios

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Http\Request;
 // RUTAS PUBLICAS
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/servicios', [TipoServicioController::class, 'getActivosParaDiego']);
+
 
 Route::get('/galeria', [GaleriaController::class, 'index']);
 Route::get('/servicios', [ServicioController::class, 'index']);
@@ -70,7 +72,6 @@ Route::middleware(['auth:sanctum', 'role:Estilista'])->group(function () {
 
 // CLIENTE
 Route::middleware(['auth:sanctum', 'role:Cliente'])->group(function () {
-    Route::get('/servicios', [TipoServicioController::class, 'getActivosParaDiego']);
     Route::get('/ver-perfil', [PerfilController::class, 'mostrarPerfil']);
     Route::patch('/editar-perfil', [PerfilController::class, 'editarPerfil']);
     Route::get('/citas/disponibilidad', [CitaController::class, 'getDisponibilidad']);
