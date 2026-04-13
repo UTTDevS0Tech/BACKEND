641da4cfcf117ec443d20c98fe7829b123978fef
Meredith Judith Valdivia Hernandez <24170085@uttcampus.edu.mx>
2026-04-12 18:31:31 -0600

    .

--- routes/api.php
+++ routes/api.php
@@ -79,7 +79,7 @@ Route::middleware(['auth:sanctum', 'role:Estilista'])->group(function () {
 
 // CLIENTE
 Route::middleware(['auth:sanctum', 'role:Cliente'])->group(function () {
-    Route::get('/servicios', [TipoServicioController::class, 'getActivosParaDiego']);
+    Route::get('/servicioss', [TipoServicioController::class, 'getActivosParaDiego']);
     Route::get('/ver-perfil', [PerfilController::class, 'mostrarPerfil']);
     Route::patch('/editar-perfil', [PerfilController::class, 'editarPerfil']);
     Route::apiResource('citas', CitaController::class);
