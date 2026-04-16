0ac2bfc13987a552f2e2ad2314ae8eeba370c396
Carlos D <carlosdanielgrdz@gmail.com>
2026-04-15 18:09:00 -0600

    tunenanodnado

--- app/Mail/CitaConfirmadaMail.php
+++ app/Mail/CitaConfirmadaMail.php
@@ -6,16 +6,39 @@ use App\Models\Cita;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Carbon\Carbon;
 
 class CitaConfirmadaMail extends Mailable
 {
     use Queueable, SerializesModels;
 
     public Cita $cita;
+    public string $googleCalendarUrl;
 
     public function __construct(Cita $cita)
     {
         $this->cita = $cita;
+
+        $inicio = Carbon::parse($cita->fecha_c . ' ' . $cita->hora_c)->format('Ymd\THis');
+        $fin = Carbon::parse($cita->fecha_c . ' ' . $cita->hora_fin)->format('Ymd\THis');
+
+        $serviciosTexto = $cita->detalles
+            ->map(function ($detalle) {
+                return $detalle->tipoServicio->nombre
+                    ?? $detalle->tipoServicio->nom
+                    ?? 'Servicio';
+            })
+            ->implode(', ');
+
+        $titulo = 'Cita en Estética Nova';
+        $descripcion = 'Tu cita ha sido agendada correctamente. Servicios: ' . $serviciosTexto;
+        $ubicacion = 'Estética Nova';
+
+        $this->googleCalendarUrl = 'https://calendar.google.com/calendar/render?action=TEMPLATE'
+            . '&text=' . urlencode($titulo)
+            . '&dates=' . urlencode($inicio . '/' . $fin)
+            . '&details=' . urlencode($descripcion)
+            . '&location=' . urlencode($ubicacion);
     }
 
     public function build()
