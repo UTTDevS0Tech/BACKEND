9265b108c569b4ec26f882e9a26a86ccb92c4272
Carlos D <carlosdanielgrdz@gmail.com>
2026-04-15 22:05:22 -0600

    NETA PINCHE VBOTON

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -1,12 +1,24 @@
-<a href="{{ url('/login') }}"
-   style="display: inline-block;
-          background-color: #c9b99a;
-          color: #ffffff;
-          text-decoration: none;
-          padding: 14px 48px;
-          border-radius: 6px;
-          font-size: 14px;
-          letter-spacing: 1px;
-          font-family: Georgia, serif;">
-   Ir al Login
+<table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="center">
+<table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="center">
+<a href="{{ $url }}"
+   style="display:inline-block;
+          background-color:#c9b99a;
+          color:#ffffff;
+          text-decoration:none;
+          padding:14px 48px;
+          border-radius:6px;
+          font-size:14px;
+          letter-spacing:1px;
+          font-family:Georgia,serif;">
+   {{ $slot }}
 </a>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
